648e58dd8880de41ccb8d9a8475c4aa4cb7b1408
Gabriel Scaramal <gscarama@mi-r1-p4.s19.be>
2024-11-29 11:32:00 +0100

    Re arranged files

--- current-account-api/src/main/java/com/capgemini/app/Service/AccountService.java
+++ current-account-api/src/main/java/com/capgemini/app/Service/AccountService.java
@@ -1,11 +1,14 @@
-package com.capgemini.app.Accounts;
+package com.capgemini.app.Service;
 
-import java.util.UUID;
-import java.util.HashMap;
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.UUID;
+
+import com.capgemini.app.Entity.Account;
+import com.capgemini.app.Entity.CurrentAccount;
 
 public class AccountService {
-	static HashMap <UUID, AAccount> accounts = new HashMap<>();
+	static HashMap <UUID, Account> accounts = new HashMap<>();
 	// UUID uuid = UUID.randomUUID();
 	// Change way of storing accounts
 
@@ -19,8 +22,11 @@ public class AccountService {
 		accounts.put(customerID, new CurrentAccount(customerID, balance));
 	}
 
-	static public AAccount getCurrentAccount (UUID customerID) {
+	static public Account getCurrentAccount (UUID customerID) {
 		// if (accounts.containsKey(customerID))
 			return accounts.get(customerID);
 	}
 }
+
+		// if (balance.compareTo(BigDecimal.ZERO) != 0)
+		// 	this.addTransaction((balance));
