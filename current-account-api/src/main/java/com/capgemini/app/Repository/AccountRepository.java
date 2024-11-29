b1a73eeca4a72a99fac6f4a8a93dde599d53407b
Scaramovistk <gabrielscaramaleu@gmail.com>
2024-11-29 15:01:16 +0100

    CRUD in AccountRepository

--- current-account-api/src/main/java/com/capgemini/app/Repository/AccountRepository.java
+++ current-account-api/src/main/java/com/capgemini/app/Repository/AccountRepository.java
@@ -8,10 +8,10 @@ import com.capgemini.app.Entity.Account;
 public class AccountRepository {
 
 	static private AccountRepository instance;
-	static HashMap <UUID, Account> accounts;
+	static private HashMap <UUID, Account> accounts = new HashMap<UUID, Account>();
 
 	private AccountRepository() {
-		accounts = new HashMap<>();
+		
 	}
 	
 	public static AccountRepository getInstance()
@@ -20,5 +20,26 @@ public class AccountRepository {
 			instance = new AccountRepository();
 		return(instance);
 	}
+
+	public Account getAccount(UUID customerID) {
+		return accounts.get(customerID);
+	}
+
+	public void saveAccount(Account account) {
+		accounts.put(account.getCustomerID(), account);
+	}
+
+	public void updateAccount(Account account) {
+		accounts.put(account.getCustomerID(), account);
+	}
+
+	public void deleteAccount(UUID customerID) {
+		accounts.remove(customerID);
+	}
+
+	public boolean checkID(UUID customerID) {
+		return accounts.containsKey(customerID);
+	}
+
 
 }
