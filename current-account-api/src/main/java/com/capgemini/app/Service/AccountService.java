f1744cfd09ed6c7d36cf1b4fab5c7e6693d7d75f
Gabriel Scaramal <gscarama@mi-r1-p4.s19.be>
2024-11-29 12:53:01 +0100

    AccountService updated with factory and repository

--- current-account-api/src/main/java/com/capgemini/app/Service/AccountService.java
+++ current-account-api/src/main/java/com/capgemini/app/Service/AccountService.java
@@ -5,28 +5,25 @@ import java.util.HashMap;
 import java.util.UUID;
 
 import com.capgemini.app.Entity.Account;
-import com.capgemini.app.Entity.CurrentAccount;
+import com.capgemini.app.Factory.AccountFactory;
+import com.capgemini.app.Repository.AccountRepository;
 
 public class AccountService {
-	static HashMap <UUID, Account> accounts = new HashMap<>();
-	// UUID uuid = UUID.randomUUID();
-	// Change way of storing accounts
 
-	public AccountService () {
+	private static AccountFactory factory = new AccountFactory();
+	private static AccountRepository repository = AccountRepository.getInstance();
+	private static HashMap<UUID, Account> accounts = new HashMap<>();
 
+	public static void createCurrentAccount(UUID customerID, BigDecimal balance) {
+		Account newAccount = factory.createAccount(customerID, "Bruce", "Lee");
+		accounts.put(customerID, newAccount);
+
+		if (!balance.equals(BigDecimal.ZERO)) {
+			newAccount.addTransaction(balance);
+		}
 	}
 
-	static public void createCurrentAccount (UUID customerID, BigDecimal balance) {
-		//Get User Information
-		// Checker if ID is valid and balance
-		accounts.put(customerID, new CurrentAccount(customerID, balance));
-	}
-
-	static public Account getCurrentAccount (UUID customerID) {
-		// if (accounts.containsKey(customerID))
-			return accounts.get(customerID);
+	public static Account getCurrentAccount(UUID customerID) {
+		return accounts.get(customerID); // Return account if it exists
 	}
 }
-
-		// if (balance.compareTo(BigDecimal.ZERO) != 0)
-		// 	this.addTransaction((balance));
