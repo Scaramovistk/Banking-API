8dc71094f93ec960c4dacf8e84f86372ee0016de
Scaramovistk <gabrielscaramaleu@gmail.com>
2024-12-02 12:32:18 +0000

    Removed hash in transaction

--- current-account-api/src/main/java/com/capgemini/app/Entity/Transaction.java
+++ current-account-api/src/main/java/com/capgemini/app/Entity/Transaction.java
@@ -6,18 +6,15 @@ public class Transaction {
 
 	String _transactionID;
 	BigDecimal _amount;
-	String _prevTxHash;
 
 	public Transaction() {
 		this._transactionID = "";
 		this._amount = new BigDecimal(0);
-		this._prevTxHash = "";
 	}
 
-	public Transaction(String transactionID, BigDecimal amount, String prevTxHash) {
+	public Transaction(String transactionID, BigDecimal amount) {
 		this._transactionID = transactionID;
 		this._amount = amount;
-		this._prevTxHash = prevTxHash;
 	}
 
 	public String getTransactionID() {
@@ -34,13 +31,5 @@ public class Transaction {
 
 	public void setAmount(BigDecimal amount) {
 		this._amount = amount;
-	}
-
-	public String getPrevTxHash() {
-		return _prevTxHash;
-	}
-
-	public void setPrevTxHash(String prevTxHash) {
-		this._prevTxHash = prevTxHash;
 	}
 }
