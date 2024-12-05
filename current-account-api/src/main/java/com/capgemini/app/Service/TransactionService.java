f5355ff6d4df3821b3bc1fb3b513e9160a2fb4d4
Scaramovistk <gabrielscaramaleu@gmail.com>
2024-12-05 11:06:24 +0100

    Transactions adapte to CurrentAccount class

--- current-account-api/src/main/java/com/capgemini/app/Service/TransactionService.java
+++ current-account-api/src/main/java/com/capgemini/app/Service/TransactionService.java
@@ -2,8 +2,9 @@ package com.capgemini.app.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.UUID;
 
-import com.capgemini.app.Entity.Account;
+import com.capgemini.app.Abstract.Account;
 import com.capgemini.app.Entity.Transaction;
 import com.capgemini.app.Factory.TransactionFactory;
 
@@ -16,13 +17,13 @@ public class TransactionService {
 		return account.getLedger();
 	}
 
-	public static Transaction getTransaction(Account account, String index) {
-		if (account == null || index == null) {
+	public static Transaction getTransaction(Account account, UUID transactionID) {
+		if (account == null || transactionID == null) {
 			throw new IllegalArgumentException("Account and index cannot be null");
 		}
 		List<Transaction> ledger = account.getLedger();
 		for (Transaction transaction : ledger) {
-			if (transaction.getTransactionID().equals(index)) {
+			if (transaction.getTransactionID().equals(transactionID.toString())) {
 				return transaction;
 			}
 		}
@@ -33,7 +34,7 @@ public class TransactionService {
 		if (account == null || amount == null) {
 			throw new IllegalArgumentException("Account and amount cannot be null");
 		}
-		Transaction transaction = TransactionFactory.createTransaction(account, amount);
+		Transaction transaction = TransactionFactory.createTransaction(account.getCustomerID(), amount);
 		return transaction;
 	}
 
